4cfbfd6977e2be60cba0f15f67b2a447fe443140
MykiellDeovennPagayonan <mykielldeovenn.pagayonan-20@cpu.edu.ph>
2023-12-02 08:53:29 +0800

    fixed: sessions data (removed temporarily)

--- src/app/components/UserCard.tsx
+++ src/app/components/UserCard.tsx
@@ -9,18 +9,11 @@ type User =
   | undefined;
 
 type Props = {
-  user: User;
   pagetype: string;
 };
 
-export default function Card({ user, pagetype }: Props) {
+export default function Card({ pagetype }: Props) {
   //console.log(user)
-
-  const greeting = user?.name ? (
-    <div className="flex flex-col items-center p-6 bg-white rounded-lg font-bold text-5xl text-black">
-      Hello {user?.name}!
-    </div>
-  ) : null;
 
   // const emailDisplay = user?.email ? (
   //     <div className="flex flex-col items-center p-6 bg-white rounded-lg font-bold text-5xl text-black">
@@ -28,22 +21,9 @@ export default function Card({ user, pagetype }: Props) {
   //     </div>
   // ) : null
 
-  const userImage = user?.image ? (
-    <Image
-      className="border-4 border-black dark:border-slate-500 drop-shadow-xl shadow-black rounded-full mx-auto mt-8"
-      src={user?.image}
-      width={100}
-      height={100}
-      alt={user?.name ?? "Profile Pic"}
-      priority={true}
-    />
-  ) : null;
 
   return (
     <section className="flex flex-col gap-4">
-      {greeting}
-      {/* {emailDisplay} */}
-      {userImage}
       <p className="text-2xl text-center">{pagetype} Page! with auth</p>
     </section>
   );
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -7,11 +7,7 @@ export default async function Home() {
   return (
     <div className="flex flex-col h-screen w-screen bg-gray-200">
       <Navbar />
-      {session ? (
-        <UserCard user={session?.user} pagetype={"Home"} />
-      ) : (
-        <h1 className="text-5xl">You Shall Not Pass!</h1>
-      )}
+      <UserCard pagetype={"Home"} />
     </div>
   );
 }
