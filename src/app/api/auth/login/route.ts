3213399f1aa691c12977ce5f19b5de4fc85ed86c
MykiellDeovennPagayonan <mykielldeovenn.pagayonan-20@cpu.edu.ph>
2023-12-02 14:33:00 +0800

    added: jwt access tokens and refresh tokens

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,22 +1,41 @@
-import { NextResponse } from "next/server"
-import { hash } from 'bcrypt'
-import { sql } from "@vercel/postgres"
-import jwt from "jsonwebtoken"
+import { NextResponse } from "next/server";
+import { compare } from "bcrypt";
+import { sql } from "@vercel/postgres";
+import jwt from "jsonwebtoken";
 
 export async function POST(request: Request) {
   try {
-    const { email, password } = await request.json()
+    const { email, password } = await request.json();
+    console.log(password)
 
-    const token = jwt.sign({
-      email,
-      password
-    }, process.env.JWT_SECRET)
+    const response = await sql`
+    SELECT * FROM users WHERE email = ${email}
+  `
+    const user = response.rows[0];
 
-    console.log(token)
-    return NextResponse.json({token: token})
+    if (response.rows.length === 0) {
+      return NextResponse.json({ message: "email or password is incorrect"})
+    }
+
+    const correctPassword = await compare(password, user.password);
+
+    if (!correctPassword) {
+      return NextResponse.json({ message: "email or password is incorrect"})
+    }
+
+    const token = jwt.sign(
+      {
+        email,
+        password,
+      },
+      process.env.JWT_ACCESS_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    console.log(token);
+    return NextResponse.json({ token: token });
   } catch (error) {
     console.log("Error:", error);
-    return NextResponse.json({message: "failure", error: error})
+    return NextResponse.json({ message: "failure", error: error });
   }
-
 }
