4f0c3cd58a13179b7a6cd3bb79c57dc75d2ce02f
Ian Clyde Tejada <ianclyde.tejada-16@cpu.edu.ph>
2023-12-02 21:33:06 +0800

    fix password text showing up

--- src/app/login/loginCard.tsx
+++ src/app/login/loginCard.tsx
@@ -1,34 +1,34 @@
-"use client"
-import { useState } from "react"
-import { Button } from "@/components/ui/button"
+"use client";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import jwt from "jsonwebtoken"
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import jwt from "jsonwebtoken";
 
 export default function LoginCard() {
-  const [ email, setEmail ] = useState<string>("")
-  const [ password, setPassword ] = useState<string>("")
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
 
   async function handleSubmit() {
-    const response = await fetch('/api/auth/login', {
-      method: 'POST',      
+    const response = await fetch("/api/auth/login", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         email: email,
-        password: password
-      })
-    }).then(res => res.json())
+        password: password,
+      }),
+    }).then((res) => res.json());
 
-    console.log(jwt.decode(response.token))
+    console.log(jwt.decode(response.token));
   }
 
   return (
@@ -40,19 +40,30 @@ export default function LoginCard() {
         <form>
           <div className="grid w-full items-center gap-4">
             <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name">Email</Label>
-              <Input id="name" placeholder="your email" onChange={(e) => setEmail(e.target.value)}/>
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                placeholder="your email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
             <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name">Password</Label>
-              <Input id="name" placeholder="Your password" onChange={(e) => setPassword(e.target.value)}/>
+              <Label htmlFor="password">Password</Label>
+              <Input
+                id="password"
+                type="password"
+                placeholder="Your password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </div>
           </div>
         </form>
       </CardContent>
       <CardFooter className="flex justify-between">
-        <Button className="w-full" onClick={() => handleSubmit()}>Log In</Button>
+        <Button className="w-full" onClick={() => handleSubmit()}>
+          Log In
+        </Button>
       </CardFooter>
     </Card>
-  )
+  );
 }
