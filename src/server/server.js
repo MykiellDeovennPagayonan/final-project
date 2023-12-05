e12c6d9ffb1d79604a3c7410b351d4326a99e545
MykiellDeovennPagayonan <mykielldeovenn.pagayonan-20@cpu.edu.ph>
2023-12-05 12:24:38 +0800

    fixed: deployment error

--- src/server/server.js
+++ src/server/server.js
@@ -1,44 +1,8 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (g && (g = 0, op[0] && (_ = 0)), _) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
-exports.__esModule = true;
-var express_1 = require("express");
-var cors_1 = require("cors");
-var app = (0, express_1["default"])();
+import express from "express";
+import cors from "cors"
+
+const app = express();
+
 // const client = new Client({
 //   host: process.env.DB_HOST,
 //   user: process.env.DB_USER,
@@ -46,21 +10,19 @@ var app = (0, express_1["default"])();
 //   password: process.env.DB_PASSWORD,
 //   database: process.env.DB_DATABASE,
 // });
-function startServer() {
-    return __awaiter(this, void 0, void 0, function () {
-        return __generator(this, function (_a) {
-            // await client.connect();
-            app
-                .use((0, cors_1["default"])())
-                .use(express_1["default"].json())
-                .get("/api/burgers", function (req, res) {
-                res.json({ burger: "delicious" });
-            })
-                .listen(3001, function () {
-                console.log("Server has started at http://localhost:3001");
-            });
-            return [2 /*return*/];
-        });
+
+async function startServer() {
+  // await client.connect();
+
+  app
+    .use(cors())
+    .use(express.json())
+    .get("/api/burgers", (req, res) => {
+      res.json({ burger: "delicious" });
+    })
+    .listen(3001, () => {
+      console.log("Server has started at http://localhost:3001");
     });
 }
+
 startServer();
