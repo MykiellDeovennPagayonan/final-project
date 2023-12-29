06cae3992b55493cf6d7fad9366834ebab444270
MykiellDeovennPagayonan <mykielldeovenn.pagayonan-20@cpu.edu.ph>
2023-12-30 01:19:21 +0800

    added: connection to dynamic routes

--- src/app/study-notes/[id]/page.tsx
+++ src/app/study-notes/[id]/page.tsx
@@ -15,11 +15,23 @@ interface StudyNoteParameter {
 
 const StudyNote: FC<StudyNoteParameter> = ({ params }) => {
   const [notesData, setNotesData] = useState<OutputData | null>()
-  console.log(params.id)
+  
+  async function buttonPress() {
+    const response = await fetch(`http://localhost:3001/api/study-notes/${params.id}`, {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({message: "hola hola!"})
+    }).then((res => res.json()))
+
+    console.log(response)
+  }
 
   return (
     <div className="flex h-screen w-screen bg-white">
       <div className="flex flex-col h-screen w-full overflow-hidden overflow-y-scroll">
+        <button onClick={() => buttonPress()}> Press Me </button>
         <h1 className="text-center mt-20"> {params.id} </h1>
         <Notes />
         <Separator className="w-3/5 mx-auto h-[3px]" />
