48d36bcbe9f1405553c1804ad866c5ab56ab885c
Jeff Andre Millan <122857926+jeffandre123@users.noreply.github.com>
2024-01-03 03:40:45 +0800

    added functionalities & design to the study group

--- src/app/studygroup/_components/displayStudyGroup.tsx
+++ src/app/studygroup/_components/displayStudyGroup.tsx
@@ -4,6 +4,8 @@ import { FC, useState, useEffect } from "react";
 import { StudyGroupCreate } from "./createStudyGroup";
 import { Separator } from "../../../components/ui/separator";
 import Link from "next/link";
+import Navbar from "@/components/Navbar";
+
 
 export const StudyGroups: FC = () => {
   const [studyGroups, setStudyGroups] = useState([]);
@@ -19,30 +21,36 @@ export const StudyGroups: FC = () => {
   }, []);
 
   return (
-    <div className="flex mr-3 flex-col">
-      <div className="flex justify-end mr-7">
-        <StudyGroupCreate />
-      </div>
+    <div className="flex flex-col h-screen w-screen bg-gray-200">
+    <Navbar />
+    <div className="flex justify-center ">
+    <div className="flex flex-col w-full max-w-screen-xl">
+        <div className="flex justify-end mr-3 mb-5">
+            <StudyGroupCreate />
+        </div>
 
-      <div className="grid gap-5 md:grid-cols-2 xl:grid-cols-4">
-        {studyGroups.map((studyGroupItem) => {
-          return (
-            <Link href={`/home${studyGroupItem.id}`} key={studyGroupItem.id}>
-              <div
-                key={studyGroupItem.id}
-                className="h-56 md:h-72 flex my-4 bg-gray-50 rounded-lg shadow-lg border border-gray-200 p-8 flex-col"
-              >
-                <p className="mt-1">{studyGroupItem.name}</p>
-                <Separator orientation="horizontal" className=" w-full" />
-                <p className="mt-3 overflow-y-auto">
-                  {" "}
-                  {studyGroupItem.description}
-                </p>
-              </div>
-            </Link>
-          );
-        })}
-      </div>
+        <div className="grid gap-5 md:grid-cols-5 xl:grid-cols-4 ">
+            {studyGroups.map((studyGroupItem) => {
+                return (
+                    <Link href={`/home${studyGroupItem.id}`} key={studyGroupItem.id}>
+                        <div
+                            key={studyGroupItem.id}
+                            className=" hover:bg-gray-200 hover:shadow-lg hover:border hover:border-gray-300 hover:text-black md:h-36 flex my-2 bg-gray-50 rounded-lg shadow-lg border border-gray-200 p-8 flex-col"
+                        >
+                            <p className="font-bold text-xl mt-1">{studyGroupItem.name}</p>
+                            <Separator orientation="horizontal" className="w-full bg-gray-300" />
+                            <p className="mt-3 overflow-y-auto">
+                                {studyGroupItem.description}
+                            </p>
+                        </div>
+                    </Link>
+                );
+            })}
+        </div>
     </div>
+</div>
+
+    </div>
+  
   );
 };
