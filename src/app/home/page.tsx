40dd180d40690f806abd2635f96b155d6cf80f15
Jeff Andre Millan <122857926+jeffandre123@users.noreply.github.com>
2024-01-03 05:07:00 +0800

    added study group page backend

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -28,14 +28,16 @@ export default function Home() {
     <div className="flex flex-col h-screen w-screen bg-gray-200">
       <Navbar />
       <div className="flex flex-col items-center justify-center">
+        
         <div className="grid grid-cols-3 space-x-4 mt-4" style={{ width: '85%' }}> 
-          <div className="col-span-2 h-40 flex items-center justify-center bg-white rounded-lg shadow-md p-4"> 
+          <div className="flex-col col-span-2 h-40 flex items-center justify-center bg-white rounded-lg shadow-md p-4"> 
+          <div className="ml-[20%] mb-1.5 text-gray-400 mr-auto">Join a Study Group:</div>
             <div className="flex flex-row w-3/5 border-2 border-gray-500 p-1 rounded-lg">
               <input
                 id="name"
                 type="text"
-                placeholder="Enter a join code"
-                className="border border-gray-300 rounded-l-lg focus:ring-2 focus:ring-green-400 flex-grow p-2"
+                placeholder="Enter code"
+                className="border border-gray-300 rounded-l-lg flex-grow p-2"
               />
               <button className="bg-green-500 border border-gray-300 text-white  rounded-r-lg px-10 h-10 hover:bg-green-400 focus:ring-2 focus:ring-green-400 focus:outline-none">
                 JOIN
