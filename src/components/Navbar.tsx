54844856b2ce3a808fa79391c3722d37d852b62a
Jeff Andre Millan <122857926+jeffandre123@users.noreply.github.com>
2024-01-05 00:56:44 +0800

    updated: navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,36 +2,51 @@
 import Link from 'next/link'
 import { Button } from './ui/button'
 import { useRouter } from 'next/navigation'
-import Image from 'next/image'
 
-export default function Navbar() {
+export default function Navbar () {
   const router = useRouter()
 
-  function logout() {
+  function logout () {
     localStorage.clear()
     router.push('/')
   }
 
   return (
-    <nav className='bg-gray-300 w-screen h-20 p-4 px-8 flex'>
-      <Link href="/home">
-        <div className="flex items-center">
-          <Image width={50} height={50} src="/scriba-landing-img-png.png" alt="Logo" className="h-12 mr-2" />
-          <span className="text-3xl font-bold text-green-500 outlined-text">Scriba</span>
-
+    <nav className='bg-gray-300 w-screen h-19 px-4 flex'>
+      <Link href='/home'>
+        <div className='flex items-center'>
+          <img
+            src='/scriba-landing-img-png.png'
+            alt='Logo'
+            className='h-12 mr-2'
+          />
+          <span className='text-3xl font-bold text-green-500 outlined-text'>
+            Scriba
+          </span>
         </div>
       </Link>
       <div className='ml-auto'>
         {' '}
         <Link href='/study-notes'>
           {' '}
-          <Button className='ml-auto mr-2 bg-gray-700 hover:bg-green-300 hover:text-black'> Study Notes </Button>
+          <Button className='w-0 md:w-12 lg:w-24 h-full bg-transparent ml-auto mr-2 text-gray-700 hover:bg-transparent focus:bg-transparent hover:text-green-600 focus:text-green-600 hover:border-b-4 focus:border-b-4 hover:rounded-none focus:rounded-none hover:border-green-900 focus:border-green-900 font-semibold py-2 px-4 focus:outline-none'>
+            <p className='hidden sm:block'>Study Notes</p>
+          </Button>
         </Link>
-        <Link href='/study-groups'>
+        <Link href='/study-notes'>
           {' '}
-          <Button className='ml-auto mr-2 bg-gray-700 hover:bg-green-300 hover:text-black'> Study Groups </Button>
+          <Button className='w-0 md:w-12 lg:w-24 h-full bg-transparent ml-auto mr-2 text-gray-700 hover:bg-transparent focus:bg-transparent hover:text-green-600 focus:text-green-600 hover:border-b-4 focus:border-b-4 hover:rounded-none focus:rounded-none hover:border-green-900 focus:border-green-900 font-semibold py-2 px-4 focus:outline-none'>
+            {' '}
+            <p className='hidden sm:block'>Study Groups</p>{' '}
+          </Button>
         </Link>
-        <Button onClick={() => logout()} className='hover:bg-green-900 hover:text-white'> Log out </Button>
+        <Button
+          onClick={() => logout()}
+          className='hover:bg-green-900 hover:text-white'
+        >
+          {' '}
+          Log out{' '}
+        </Button>
       </div>
     </nav>
   )
